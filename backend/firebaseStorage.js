9b065acd51c2e64af8bd5045c3e19bd1ca3ed514
Abdu Jemal <abdu1994jemal@gmail.com>
2024-08-28 04:22:36 +0300

    deployment fix 17

--- backend/firebaseStorage.js
+++ backend/firebaseStorage.js
@@ -1,33 +1,28 @@
-// services/firebaseService.js
 import admin from 'firebase-admin';
 import { v4 as uuidv4 } from 'uuid';
-import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 export const uploadImageToFirebase = async (file) => {
-    const filePath = path.join(__dirname, 'uploads', file.filename);
+  try {
+    const storageRef = admin.storage().bucket();
     const filename = `${Date.now()}_${file.originalname}`;
-    const bucket = admin.storage().bucket();
-    try {
-        const file = bucket.file(`uploads/${filename}`);
 
-        await file.save(fs.readFileSync(filePath), {
-        metadata: {
-            contentType: 'image/jpeg',  // Adjust the content type according to the file type
-            firebaseStorageDownloadTokens: uuidv4(),
-        },
-        });
+    // Create a reference to the file in Firebase Storage
+    const fileRef = storageRef.file(`uploads/${filename}`);
 
-        const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(file.name)}?alt=media`;
+    // Upload the file buffer directly to Firebase Storage
+    await fileRef.save(file.buffer, {
+      metadata: {
+        contentType: file.mimetype,
+        firebaseStorageDownloadTokens: uuidv4(),
+      },
+    });
 
-        return publicUrl;
-    } catch (error) {
-        console.error('Error uploading file to Firebase:', error);
-        throw error;
-    }
+    // Construct the public URL for the file
+    const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${storageRef.name}/o/${encodeURIComponent(fileRef.name)}?alt=media`;
+
+    return imageUrl;
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    throw new Error('Failed to upload file to Firebase Storage');
+  }
 };
